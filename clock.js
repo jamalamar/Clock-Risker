903a5e53092b503de798c5a3026baf07f96ed4ee
jamalamar <jamal_ab22@hotmail.com>
2019-09-19 00:59:08 -0500

    working clock from scratch

--- clock.js
+++ clock.js
@@ -1,10 +1,6 @@
 
-let seconds = 0
-let minutes = 0
-let hours = 0
-
-
-function Clock (){
+//Function that returns hours
+function ClockHours (){
 	let seconds = Date.now() / 1000
 	let days = seconds / 86400
 	let years = days / 365.25
@@ -21,25 +17,62 @@ function Clock (){
 	let secondsNow = secondsPercentage * 60
 
 
-
-	return secondsNow
+	return Math.trunc(hoursNow)
 }
 
+
+
+//Function that returns clock minutes
+function ClockMinutes (){
+	let seconds = Date.now() / 1000
+	let days = seconds / 86400
+	let years = days / 365.25
+
+	let time = days - Math.floor(days)
+
+	//returns hour of the day
+	let hoursNow = time * 24
+	//returns the percentage of the hour of the day
+	let minutesPercentage = hoursNow - Math.floor(hoursNow)
+	//returns minutes of the hour
+	let minutesNow = minutesPercentage * 60
+	let secondsPercentage = minutesNow - Math.floor(minutesNow)
+	let secondsNow = secondsPercentage * 60
+
+
+	return Math.trunc(minutesNow)
+}
+
+
+
+//Returuns clock seconds
+function ClockSeconds (){
+	let seconds = Date.now() / 1000
+	let days = seconds / 86400
+	let years = days / 365.25
+
+	let time = days - Math.floor(days)
+
+	//returns hour of the day
+	let hoursNow = time * 24
+	//returns the percentage of the hour of the day
+	let minutesPercentage = hoursNow - Math.floor(hoursNow)
+	//returns minutes of the hour
+	let minutesNow = minutesPercentage * 60
+	let secondsPercentage = minutesNow - Math.floor(minutesNow)
+	let secondsNow = secondsPercentage * 60
+
+
+	return Math.trunc(secondsNow)
+}
 
 
 
 function timestamp(){
-	console.log(Clock())
+	console.log("Hours:" + ClockHours() + "  Minutes:" + ClockMinutes() + "  Seconds:" + ClockSeconds())
+
 	setTimeout(timestamp, 1000)
 }
 
 timestamp()
-
-
-
-
-
-// let today = new Date()
-// console.log(today)
-
 
